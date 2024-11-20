35e519260f3682ae295d3e2828b38ad1821d97bb
dyson-yamashita <dyson-yamashita@users.noreply.github.com>
2024-11-20 00:41:38 +0000

    deploy: 9d8f198ece6b7289fd283f54d002e5b5325294de

---
+++ assets/5_ObjectiveBox.stories-BnSqc_UB.js
@@ -0,0 +1,110 @@
+import{j as _}from"./jsx-runtime-DFnSfiB4.js";import{R as j}from"./index-DQ2WTIsS.js";import{w as m,e,u as a,f as Y}from"./index-DQLiMaGX.js";import{O as z}from"./ObjectiveBox-CnhuyFyi.js";import{e as c}from"./Comment-DRhAEK4Z.js";function A(){var o="/home/runner/work/pufu-editor/pufu-editor/src/tests/Objective.stories.test.ts",n="79a536f47e0ceec940ce5a742b53790c74a16d17",t=window,i="__coverage__",O={path:"/home/runner/work/pufu-editor/pufu-editor/src/tests/Objective.stories.test.ts",statementMap:{0:{start:{line:2,column:31},end:{line:21,column:1}},1:{start:{line:5,column:17},end:{line:5,column:38}},2:{start:{line:6,column:2},end:{line:8,column:24}},3:{start:{line:9,column:2},end:{line:11,column:24}},4:{start:{line:12,column:2},end:{line:12,column:64}},5:{start:{line:13,column:2},end:{line:15,column:24}},6:{start:{line:16,column:18},end:{line:18,column:4}},7:{start:{line:19,column:2},end:{line:19,column:45}},8:{start:{line:20,column:2},end:{line:20,column:44}},9:{start:{line:22,column:32},end:{line:38,column:1}},10:{start:{line:25,column:17},end:{line:25,column:38}},11:{start:{line:26,column:2},end:{line:28,column:24}},12:{start:{line:29,column:2},end:{line:29,column:64}},13:{start:{line:30,column:2},end:{line:32,column:24}},14:{start:{line:33,column:18},end:{line:35,column:4}},15:{start:{line:36,column:2},end:{line:36,column:45}},16:{start:{line:37,column:2},end:{line:37,column:44}},17:{start:{line:39,column:28},end:{line:93,column:1}},18:{start:{line:40,column:17},end:{line:40,column:38}},19:{start:{line:41,column:2},end:{line:43,column:24}},20:{start:{line:44,column:2},end:{line:46,column:24}},21:{start:{line:47,column:18},end:{line:49,column:4}},22:{start:{line:50,column:2},end:{line:50,column:33}},23:{start:{line:51,column:22},end:{line:53,column:4}},24:{start:{line:54,column:2},end:{line:54,column:37}},25:{start:{line:55,column:22},end:{line:57,column:4}},26:{start:{line:58,column:2},end:{line:58,column:42}},27:{start:{line:59,column:24},end:{line:61,column:4}},28:{start:{line:62,column:2},end:{line:62,column:51}},29:{start:{line:63,column:2},end:{line:63,column:55}},30:{start:{line:64,column:21},end:{line:66,column:4}},31:{start:{line:67,column:19},end:{line:69,column:4}},32:{start:{line:70,column:2},end:{line:70,column:34}},33:{start:{line:71,column:16},end:{line:73,column:4}},34:{start:{line:74,column:2},end:{line:74,column:31}},35:{start:{line:75,column:2},end:{line:75,column:42}},36:{start:{line:76,column:2},end:{line:76,column:44}},37:{start:{line:77,column:2},end:{line:77,column:45}},38:{start:{line:78,column:14},end:{line:80,column:4}},39:{start:{line:81,column:2},end:{line:81,column:32}},40:{start:{line:82,column:2},end:{line:82,column:29}},41:{start:{line:83,column:2},end:{line:83,column:46}},42:{start:{line:84,column:2},end:{line:84,column:40}},43:{start:{line:85,column:2},end:{line:85,column:45}},44:{start:{line:86,column:15},end:{line:88,column:4}},45:{start:{line:89,column:2},end:{line:89,column:30}},46:{start:{line:90,column:2},end:{line:90,column:46}},47:{start:{line:91,column:2},end:{line:91,column:44}},48:{start:{line:92,column:2},end:{line:92,column:41}},49:{start:{line:94,column:27},end:{line:107,column:1}},50:{start:{line:95,column:17},end:{line:95,column:38}},51:{start:{line:96,column:2},end:{line:96,column:68}},52:{start:{line:97,column:18},end:{line:99,column:4}},53:{start:{line:100,column:2},end:{line:100,column:46}},54:{start:{line:101,column:2},end:{line:101,column:57}},55:{start:{line:102,column:24},end:{line:104,column:4}},56:{start:{line:105,column:2},end:{line:105,column:52}},57:{start:{line:106,column:2},end:{line:106,column:58}},58:{start:{line:108,column:28},end:{line:116,column:1}},59:{start:{line:109,column:17},end:{line:109,column:38}},60:{start:{line:110,column:18},end:{line:112,column:4}},61:{start:{line:113,column:2},end:{line:113,column:51}},62:{start:{line:114,column:2},end:{line:114,column:49}},63:{start:{line:115,column:2},end:{line:115,column:51}},64:{start:{line:117,column:29},end:{line:125,column:1}},65:{start:{line:118,column:17},end:{line:118,column:38}},66:{start:{line:119,column:18},end:{line:121,column:4}},67:{start:{line:122,column:2},end:{line:122,column:51}},68:{start:{line:123,column:2},end:{line:123,column:53}},69:{start:{line:124,column:2},end:{line:124,column:47}}},fnMap:{0:{name:"(anonymous_0)",decl:{start:{line:2,column:31},end:{line:2,column:32}},loc:{start:{line:4,column:6},end:{line:21,column:1}},line:4},1:{name:"(anonymous_1)",decl:{start:{line:22,column:32},end:{line:22,column:33}},loc:{start:{line:24,column:6},end:{line:38,column:1}},line:24},2:{name:"(anonymous_2)",decl:{start:{line:39,column:28},end:{line:39,column:29}},loc:{start:{line:39,column:57},end:{line:93,column:1}},line:39},3:{name:"(anonymous_3)",decl:{start:{line:94,column:27},end:{line:94,column:28}},loc:{start:{line:94,column:56},end:{line:107,column:1}},line:94},4:{name:"(anonymous_4)",decl:{start:{line:108,column:28},end:{line:108,column:29}},loc:{start:{line:108,column:57},end:{line:116,column:1}},line:108},5:{name:"(anonymous_5)",decl:{start:{line:117,column:29},end:{line:117,column:30}},loc:{start:{line:117,column:58},end:{line:125,column:1}},line:117}},branchMap:{},s:{0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,21:0,22:0,23:0,24:0,25:0,26:0,27:0,28:0,29:0,30:0,31:0,32:0,33:0,34:0,35:0,36:0,37:0,38:0,39:0,40:0,41:0,42:0,43:0,44:0,45:0,46:0,47:0,48:0,49:0,50:0,51:0,52:0,53:0,54:0,55:0,56:0,57:0,58:0,59:0,60:0,61:0,62:0,63:0,64:0,65:0,66:0,67:0,68:0,69:0},f:{0:0,1:0,2:0,3:0,4:0,5:0},b:{},inputSourceMap:{version:3,file:null,sources:["/home/runner/work/pufu-editor/pufu-editor/src/tests/Objective.stories.test.ts"],names:[],mappings:"AAAA,SAAS,QAAQ,WAAW,cAAc;AAMnC,aAAM,kBAAkB,OAAO;AAAA,EACpC;AACF,MAA0B;AACxB,QAAM,SAAS,OAAO,aAAa;AACnC;AAAA,IACE,MAAM,OAAO,WAAW,gBAAgB,EAAE,MAAM,MAAM,CAAC;AAAA,EACzD,EAAE,kBAAkB;AAEpB;AAAA,IACE,MAAM,OAAO,WAAW,gBAAgB,EAAE,MAAM,MAAM,CAAC;AAAA,EACzD,EAAE,kBAAkB;AACpB,SAAO,MAAM,OAAO,WAAW,QAAQ,CAAC,EAAE,kBAAkB;AAC5D;AAAA,IACE,MAAM,OAAO,sBAAsB,SAAS;AAAA,EAC9C,EAAE,kBAAkB;AAEpB,QAAM,UAAU,MAAM,OAAO,WAAW,gBAAgB;AAAA,IACtD,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,KAAK,SAAS,WAAW;AACzC,SAAO,sBAAsB,WAAW;AAC1C;AAEO,aAAM,mBAAmB,OAAO;AAAA,EACrC;AACF,MAA0B;AACxB,QAAM,SAAS,OAAO,aAAa;AACnC;AAAA,IACE,MAAM,OAAO,WAAW,iBAAiB,EAAE,MAAM,MAAM,CAAC;AAAA,EAC1D,EAAE,kBAAkB;AACpB,SAAO,MAAM,OAAO,WAAW,QAAQ,CAAC,EAAE,kBAAkB;AAC5D;AAAA,IACE,MAAM,OAAO,sBAAsB,SAAS;AAAA,EAC9C,EAAE,kBAAkB;AACpB,QAAM,UAAU,MAAM,OAAO,WAAW,iBAAiB;AAAA,IACvD,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,KAAK,SAAS,WAAW;AACzC,SAAO,sBAAsB,WAAW;AAC1C;AAEO,aAAM,eAAe,OAAO,EAAE,cAAc,MAA0B;AAC3E,QAAM,SAAS,OAAO,aAAa;AAEnC;AAAA,IACE,MAAM,OAAO,WAAW,iBAAiB,EAAE,MAAM,MAAM,CAAC;AAAA,EAC1D,EAAE,kBAAkB;AACpB;AAAA,IACE,MAAM,OAAO,mBAAmB,aAAa;AAAA,EAC/C,EAAE,kBAAkB;AAEpB,QAAM,UAAU,MAAM,OAAO,WAAW,iBAAiB;AAAA,IACvD,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,MAAM,OAAO;AAE7B,QAAM,cAAc,MAAM,OAAO,WAAW,iBAAiB;AAAA,IAC3D,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,MAAM,WAAW;AAEjC,QAAM,cAAc,MAAM,OAAO,WAAW,iBAAiB;AAAA,IAC3D,MAAM;AAAA,EACR,CAAC;AACD,SAAO,WAAW,EAAE,kBAAkB;AACtC,QAAM,gBAAgB,MAAM,OAAO,WAAW,yBAAyB;AAAA,IACrE,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,KAAK,eAAe,WAAW;AAC/C,SAAO,MAAM,OAAO,mBAAmB,WAAW,CAAC;AAEnD,QAAM,aAAa,MAAM,OAAO,WAAW,yBAAyB;AAAA,IAClE,MAAM;AAAA,EACR,CAAC;AACD,QAAM,WAAW,MAAM,OAAO,WAAW,yBAAyB;AAAA,IAChE,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,MAAM,QAAQ;AAC9B,QAAM,QAAQ,MAAM,OAAO,WAAW,yBAAyB;AAAA,IAC7D,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,MAAM,KAAK;AAC3B,SAAO,UAAU,EAAE,YAAY,OAAO;AACtC,SAAO,UAAU,EAAE,IAAI,YAAY,KAAK;AACxC,SAAO,UAAU,EAAE,IAAI,YAAY,MAAM;AACzC,QAAM,MAAM,MAAM,OAAO,WAAW,yBAAyB;AAAA,IAC3D,MAAM;AAAA,EACR,CAAC;AACD,SAAO,GAAG,EAAE;AACZ,QAAM,UAAU,MAAM,GAAG;AACzB,SAAO,UAAU,EAAE,IAAI,YAAY,OAAO;AAC1C,SAAO,UAAU,EAAE,YAAY,KAAK;AACpC,SAAO,UAAU,EAAE,IAAI,YAAY,MAAM;AAEzC,QAAM,OAAO,MAAM,OAAO,WAAW,yBAAyB;AAAA,IAC5D,MAAM;AAAA,EACR,CAAC;AACD,QAAM,UAAU,MAAM,IAAI;AAC1B,SAAO,UAAU,EAAE,IAAI,YAAY,OAAO;AAC1C,SAAO,UAAU,EAAE,IAAI,YAAY,KAAK;AACxC,SAAO,UAAU,EAAE,YAAY,MAAM;AACvC;AAEO,aAAM,cAAc,OAAO,EAAE,cAAc,MAA0B;AAC1E,QAAM,SAAS,OAAO,aAAa;AAEnC,SAAO,MAAM,OAAO,WAAW,iBAAiB,EAAE,MAAM,MAAM,CAAC,CAAC;AAEhE,QAAM,UAAU,MAAM,OAAO,WAAW,iBAAiB;AAAA,IACvD,MAAM;AAAA,EACR,CAAC;AACD,SAAO,OAAO,EAAE,gBAAgB,UAAU;AAC1C,SAAO,MAAM,OAAO,mBAAmB,aAAa,CAAC;AAErD,QAAM,gBAAgB,MAAM,OAAO,WAAW,yBAAyB;AAAA,IACrE,MAAM;AAAA,EACR,CAAC;AACD,SAAO,aAAa,EAAE,gBAAgB,UAAU;AAChD,SAAO,MAAM,OAAO,mBAAmB,cAAc,CAAC;AACxD;AAEO,aAAM,eAAe,OAAO,EAAE,cAAc,MAA0B;AAC3E,QAAM,SAAS,OAAO,aAAa;AACnC,QAAM,UAAU,MAAM,OAAO,WAAW,iBAAiB;AAAA,IACvD,MAAM;AAAA,EACR,CAAC;AACD,QAAM,OAAO,OAAO,EAAE,IAAI,YAAY,SAAS;AAC/C,QAAM,OAAO,OAAO,EAAE,YAAY,WAAW;AAC7C,QAAM,OAAO,OAAO,EAAE,IAAI,YAAY,SAAS;AACjD;AAEO,aAAM,gBAAgB,OAAO,EAAE,cAAc,MAA0B;AAC5E,QAAM,SAAS,OAAO,aAAa;AACnC,QAAM,UAAU,MAAM,OAAO,WAAW,iBAAiB;AAAA,IACvD,MAAM;AAAA,EACR,CAAC;AACD,QAAM,OAAO,OAAO,EAAE,IAAI,YAAY,SAAS;AAC/C,QAAM,OAAO,OAAO,EAAE,IAAI,YAAY,WAAW;AACjD,QAAM,OAAO,OAAO,EAAE,YAAY,SAAS;AAC7C;"},_coverageSchema:"1a1c01bbd47fc00a2c39e90264f33305004495a9",hash:"79a536f47e0ceec940ce5a742b53790c74a16d17"},s=t[i]||(t[i]={});(!s[o]||s[o].hash!==n)&&(s[o]=O);var l=s[o];return A=function(){return l},l}A();A().s[0]++;const V=async({canvasElement:o})=>{A().f[0]++;const n=(A().s[1]++,m(o));A().s[2]++,e(await n.findByRole("gaining-goal",{name:"box"})).toBeInTheDocument(),A().s[3]++,e(await n.findByRole("gaining-goal",{name:"box"})).toBeInTheDocument(),A().s[4]++,e(await n.findByText(/^獲得目標$/)).toBeInTheDocument(),A().s[5]++,e(await n.findByPlaceholderText("獲得目標を入力")).toBeInTheDocument();const t=(A().s[6]++,await n.findByRole("gaining-goal",{name:"textbox"}));A().s[7]++,await a.type(t,"獲得目標のテキスト"),A().s[8]++,n.findAllByDisplayValue("獲得目標のテキスト")};A().s[9]++;const L=async({canvasElement:o})=>{A().f[1]++;const n=(A().s[10]++,m(o));A().s[11]++,e(await n.findByRole("win-condition",{name:"box"})).toBeInTheDocument(),A().s[12]++,e(await n.findByText(/^勝利条件$/)).toBeInTheDocument(),A().s[13]++,e(await n.findByPlaceholderText("勝利条件を入力")).toBeInTheDocument();const t=(A().s[14]++,await n.findByRole("win-condition",{name:"textbox"}));A().s[15]++,await a.type(t,"勝利条件のテキスト"),A().s[16]++,n.findAllByDisplayValue("勝利条件のテキスト")};A().s[17]++;const F=async({canvasElement:o})=>{A().f[2]++;const n=(A().s[18]++,m(o));A().s[19]++,e(await n.findByRole("win-condition",{name:"box"})).toBeInTheDocument(),A().s[20]++,e(await n.findByDisplayValue("サンプルテキストです。")).toBeInTheDocument();const t=(A().s[21]++,await n.findByRole("win-condition",{name:"textbox"}));A().s[22]++,await a.click(t);const i=(A().s[23]++,await n.findByRole("win-condition",{name:"comment-icon"}));A().s[24]++,await a.click(i);const O=(A().s[25]++,await n.findByRole("win-condition",{name:"comment"}));A().s[26]++,e(O).toBeInTheDocument();const s=(A().s[27]++,await n.findByRole("win-condition-comment",{name:"textbox"}));A().s[28]++,await a.type(s,"勝利条件のコメント"),A().s[29]++,e(await n.findByDisplayValue("勝利条件のコメント"));const l=(A().s[30]++,await n.findByRole("win-condition-comment",{name:"box"})),G=(A().s[31]++,await n.findByRole("win-condition-comment",{name:"palette-icon"}));A().s[32]++,await a.click(G);const H=(A().s[33]++,await n.findByRole("win-condition-comment",{name:"color-icon-green"}));A().s[34]++,await a.click(H),A().s[35]++,e(l).toHaveClass(/green/),A().s[36]++,e(l).not.toHaveClass(/red/),A().s[37]++,e(l).not.toHaveClass(/blue/);const E=(A().s[38]++,await n.findByRole("win-condition-comment",{name:"color-icon-red"}));A().s[39]++,e(E).toBeInTheDocument,A().s[40]++,await a.click(E),A().s[41]++,e(l).not.toHaveClass(/green/),A().s[42]++,e(l).toHaveClass(/red/),A().s[43]++,e(l).not.toHaveClass(/blue/);const K=(A().s[44]++,await n.findByRole("win-condition-comment",{name:"color-icon-blue"}));A().s[45]++,await a.click(K),A().s[46]++,e(l).not.toHaveClass(/green/),A().s[47]++,e(l).not.toHaveClass(/red/),A().s[48]++,e(l).toHaveClass(/blue/)};A().s[49]++;const P=async({canvasElement:o})=>{A().f[3]++;const n=(A().s[50]++,m(o));A().s[51]++,e(await n.findByRole("win-condition",{name:"box"}));const t=(A().s[52]++,await n.findByRole("win-condition",{name:"textbox"}));A().s[53]++,e(t).toHaveAttribute("readonly"),A().s[54]++,e(await n.findByDisplayValue("サンプルテキストです。"));const i=(A().s[55]++,await n.findByRole("win-condition-comment",{name:"textbox"}));A().s[56]++,e(i).toHaveAttribute("readonly"),A().s[57]++,e(await n.findByDisplayValue("コメントのテキストです。"))};A().s[58]++;const $=async({canvasElement:o})=>{A().f[4]++;const n=(A().s[59]++,m(o)),t=(A().s[60]++,await n.findByRole("win-condition",{name:"textbox"}));A().s[61]++,await e(t).not.toHaveClass("text-sm"),A().s[62]++,await e(t).toHaveClass("text-base"),A().s[63]++,await e(t).not.toHaveClass("text-lg")};A().s[64]++;const q=async({canvasElement:o})=>{A().f[5]++;const n=(A().s[65]++,m(o)),t=(A().s[66]++,await n.findByRole("win-condition",{name:"textbox"}));A().s[67]++,await e(t).not.toHaveClass("text-sm"),A().s[68]++,await e(t).not.toHaveClass("text-base"),A().s[69]++,await e(t).toHaveClass("text-lg")};c.changeLanguage("ja");const Z={title:"pufu-editor/ObjectiveBox",component:z,parameters:{layout:"centered"},tags:["autodocs"]},u={args:{label:"GainingGoal",goal:{uuid:"123",text:"",comment:{color:"blue",text:"コメントのサンプルテキストです。"}},i18n:c},render:function({...n}){const[t,i]=j.useState({uuid:"123",text:"",comment:{color:"blue",text:"コメントのサンプルテキストです。"}});return _.jsx(Z.component,{...n,goal:t,label:"GainingGoal",onChange:i,i18n:c})},play:V},r={args:{label:"WinCondition",goal:{uuid:"123",text:"",comment:{color:"blue",text:"コメントのサンプルテキストです。"}},onChange:Y(),i18n:c},play:L},C={args:{label:"WinCondition",goal:{uuid:"123",text:"サンプルテキストです。",comment:{color:"blue",text:""}},onChange:Y(),feedback:!0,i18n:c},play:F},d={args:{label:"WinCondition",goal:{uuid:"123",text:"サンプルテキストです。",comment:{color:"blue",text:"コメントのテキストです。"}},feedback:!0,preview:!0,i18n:c},play:P},M={args:{label:"WinCondition",goal:{uuid:"123",text:"サンプルテキストです。",comment:{color:"blue",text:"コメントのテキストです。"}},feedback:!0,preview:!0,i18n:c,textSize:"base"},play:$},B={args:{label:"WinCondition",goal:{uuid:"123",text:"サンプルテキストです。",comment:{color:"blue",text:"コメントのテキストです。"}},feedback:!0,preview:!0,i18n:c,textSize:"large"},play:q};var g,x,p;u.parameters={...u.parameters,docs:{...(g=u.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    label: "GainingGoal",
+    goal: {
+      uuid: "123",
+      text: "",
+      comment: {
+        color: "blue",
+        text: "コメントのサンプルテキストです。"
+      }
+    },
+    i18n: i18n
+  },
+  render: function Comp({
+    ...args
+  }) {
+    const [goal, setGoal] = React.useState<ObjectiveModel>({
+      uuid: "123",
+      text: "",
+      comment: {
+        color: "blue",
+        text: "コメントのサンプルテキストです。"
+      }
+    });
+    return <meta.component {...args} goal={goal} label={"GainingGoal"} onChange={setGoal} i18n={i18n}></meta.component>;
+  },
+  play: gainingGoalTest
+}`,...(p=(x=u.parameters)==null?void 0:x.docs)==null?void 0:p.source}}};var b,y,W;r.parameters={...r.parameters,docs:{...(b=r.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    label: "WinCondition",
+    goal: {
+      uuid: "123",
+      text: "",
+      comment: {
+        color: "blue",
+        text: "コメントのサンプルテキストです。"
+      }
+    },
+    onChange: fn(),
+    i18n: i18n
+  },
+  play: winConditionTest
+}`,...(W=(y=r.parameters)==null?void 0:y.docs)==null?void 0:W.source}}};var w,f,S;C.parameters={...C.parameters,docs:{...(w=C.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    label: "WinCondition",
+    goal: {
+      uuid: "123",
+      text: "サンプルテキストです。",
+      comment: {
+        color: "blue",
+        text: ""
+      }
+    },
+    onChange: fn(),
+    feedback: true,
+    i18n: i18n
+  },
+  play: feedbackTest
+}`,...(S=(f=C.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var v,D,I;d.parameters={...d.parameters,docs:{...(v=d.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    label: "WinCondition",
+    goal: {
+      uuid: "123",
+      text: "サンプルテキストです。",
+      comment: {
+        color: "blue",
+        text: "コメントのテキストです。"
+      }
+    },
+    feedback: true,
+    preview: true,
+    i18n: i18n
+  },
+  play: previewTest
+}`,...(I=(D=d.parameters)==null?void 0:D.docs)==null?void 0:I.source}}};var U,Q,k;M.parameters={...M.parameters,docs:{...(U=M.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    label: "WinCondition",
+    goal: {
+      uuid: "123",
+      text: "サンプルテキストです。",
+      comment: {
+        color: "blue",
+        text: "コメントのテキストです。"
+      }
+    },
+    feedback: true,
+    preview: true,
+    i18n: i18n,
+    textSize: "base"
+  },
+  play: TextBaseTest
+}`,...(k=(Q=M.parameters)==null?void 0:Q.docs)==null?void 0:k.source}}};var R,h,T;B.parameters={...B.parameters,docs:{...(R=B.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    label: "WinCondition",
+    goal: {
+      uuid: "123",
+      text: "サンプルテキストです。",
+      comment: {
+        color: "blue",
+        text: "コメントのテキストです。"
+      }
+    },
+    feedback: true,
+    preview: true,
+    i18n: i18n,
+    textSize: "large"
+  },
+  play: TextLargeTest
+}`,...(T=(h=B.parameters)==null?void 0:h.docs)==null?void 0:T.source}}};const eA=["GainingGoal","WinCondition","Feedback","Preview","TextBase","TextLarge"];export{C as Feedback,u as GainingGoal,d as Preview,M as TextBase,B as TextLarge,r as WinCondition,eA as __namedExportsOrder,Z as default};
+//# sourceMappingURL=5_ObjectiveBox.stories-BnSqc_UB.js.map
