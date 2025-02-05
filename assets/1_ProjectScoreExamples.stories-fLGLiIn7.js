cf249cde38beb14c870d4ee0cb29bb6261de4a45
dyson-yamashita <dyson-yamashita@users.noreply.github.com>
2025-02-05 00:41:51 +0000

    deploy: c55d5da4278efa0ab514eba75b14dec9f397ba5a

---
+++ assets/1_ProjectScoreExamples.stories-fLGLiIn7.js
@@ -0,0 +1,117 @@
+import{j as e}from"./jsx-runtime-DFnSfiB4.js";import{R as s}from"./index-DQ2WTIsS.js";import{i as R}from"./Comment-CLRktOpt.js";import{P as L,g as S,s as f}from"./ProjectScore-BDHpq3hS.js";import{e as B,D as l,I as C,M as w,L as N}from"./common-7oSWtkH5.js";import"./EightElements-CK4dmmlp.js";import"./Measure-KdatraD6.js";import"./index-Ce-rDAX5.js";import"./IntermediatePurpose-CeZnclaa.js";import"./ObjectiveBox-DqMJMEJU.js";const T={title:"pufu-editor/Examples",component:L,parameters:{layout:"fullscreen",docs:{description:{component:`
+プ譜エディターは書籍『予定通り進まないプロジェクトの進め方』（著: 前田考歩、後藤洋平）で<br>
+紹介されているプロジェクト譜（プ譜）を Web ブラウザで編集・表示するためのエディターです。<br>
+Github: <a href="https://github.com/goto-lab/pufu-editor" target="_blank">goto-lab/pufu-editor</a>`}}},tags:["autodocs"]},c="example1",n="example2",D=()=>!!(window.matchMedia&&window.matchMedia("(max-device-width: 640px)").matches),u={args:{uniqueKey:c,initScore:B,preview:!0,mobile:D()},render:function({...g}){const[d,r]=s.useState(!1),[i,m]=s.useState(""),[x,a]=s.useState("");return e.jsxs(e.Fragment,{children:[e.jsx(l,{scoreKey:c,kind:"json"}),e.jsx(l,{scoreKey:c,kind:"png"}),e.jsx(l,{scoreKey:c,kind:"svg"}),e.jsx(C,{onClick:()=>{const t=S(c);m(t),r(!0),a("")}}),e.jsxs("p",{className:"ml-8",children:["ID: ",c]}),e.jsx(T.component,{...g}),e.jsx(w,{open:d,text:i,error:x,onCancel:()=>r(!1),onChange:t=>{m(t)},onOk:()=>{try{a(""),f(c,i),r(!1)}catch(t){console.error(t),t instanceof Error&&a(t.message)}}})]})}},p={args:{uniqueKey:n,feedback:!0,mobile:D()},render:function({...g}){const[d,r]=s.useState(!1),[i,m]=s.useState(""),[x,a]=s.useState(""),[t,K]=s.useState(!1),[k,y]=s.useState("");return e.jsxs(e.Fragment,{children:[e.jsx(l,{scoreKey:n,kind:"json"}),e.jsx(l,{scoreKey:n,kind:"png"}),e.jsx(l,{scoreKey:n,kind:"svg"}),e.jsx(C,{onClick:()=>{const o=S(n);m(o),r(!0),a("")}}),e.jsx(N,{checked:t,onClick:o=>{K(o)},onLoad:()=>{const o=localStorage.getItem(n);o&&f(n,o);const j=localStorage.getItem(`${n}-memo`);j&&y(j)},onSave:()=>{const o=S(n);o&&localStorage.setItem(n,o),localStorage.setItem(`${n}-memo`,k)}}),e.jsxs("p",{className:"ml-8",children:["ID: ",n]}),e.jsx(T.component,{...g}),e.jsx("div",{className:"p-2",children:e.jsx(R,{className:`
+              block p-2.5 w-full text-sm text-gray-900 bg-gray-50
+              rounded-lg border border-gray-300
+              focus:outline-none focus:ring-blue-300 focus:border-blue-300
+            `,minRows:2,value:k,onChange:o=>y(o.target.value),placeholder:"メモを入力"})}),e.jsx(w,{open:d,text:i,error:x,onCancel:()=>r(!1),onChange:o=>{m(o)},onOk:()=>{try{a(""),f(n,i),r(!1)}catch(o){console.error(o),o instanceof Error&&a(o.message)}}})]})}};var b,I,h;u.parameters={...u.parameters,docs:{...(b=u.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    uniqueKey: key1,
+    initScore: exampleData,
+    preview: true,
+    mobile: isMobile()
+  },
+  render: function Comp({
+    ...args
+  }) {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [scoreText, setScoreText] = React.useState("");
+    const [error, setError] = React.useState("");
+    return <>
+        <DownloadButton scoreKey={key1} kind="json" />
+        <DownloadButton scoreKey={key1} kind="png" />
+        <DownloadButton scoreKey={key1} kind="svg" />
+        <ImportButton onClick={() => {
+        const json = getScoreJson(key1);
+        setScoreText(json!);
+        setIsOpen(true);
+        setError("");
+      }} />
+        <p className="ml-8">ID: {key1}</p>
+        <meta.component {...args}></meta.component>
+        <ModalDialog open={isOpen} text={scoreText} error={error} onCancel={() => setIsOpen(false)} onChange={(text: string) => {
+        setScoreText(text);
+      }} onOk={() => {
+        try {
+          setError("");
+          setScore(key1, scoreText);
+          setIsOpen(false);
+        } catch (e) {
+          console.error(e);
+          if (e instanceof Error) {
+            setError(e.message);
+          }
+        }
+      }} />
+      </>;
+  }
+}`,...(h=(I=u.parameters)==null?void 0:I.docs)==null?void 0:h.source}}};var v,E,O;p.parameters={...p.parameters,docs:{...(v=p.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    uniqueKey: key2,
+    feedback: true,
+    mobile: isMobile()
+  },
+  render: function Comp({
+    ...args
+  }) {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [scoreText, setScoreText] = React.useState("");
+    const [error, setError] = React.useState("");
+    const [isLocalSave, setIsLocalSave] = React.useState(false);
+    const [memo, setMemo] = React.useState("");
+    return <>
+        <DownloadButton scoreKey={key2} kind="json" />
+        <DownloadButton scoreKey={key2} kind="png" />
+        <DownloadButton scoreKey={key2} kind="svg" />
+        <ImportButton onClick={() => {
+        const json = getScoreJson(key2);
+        setScoreText(json!);
+        setIsOpen(true);
+        setError("");
+      }} />
+        <LocalSave checked={isLocalSave} onClick={(status: boolean) => {
+        setIsLocalSave(status);
+      }} onLoad={() => {
+        const json = localStorage.getItem(key2);
+        if (json) {
+          setScore(key2, json);
+        }
+        const localStorageMemo = localStorage.getItem(\`\${key2}-memo\`);
+        if (localStorageMemo) {
+          setMemo(localStorageMemo);
+        }
+      }} onSave={() => {
+        const json = getScoreJson(key2);
+        if (json) {
+          localStorage.setItem(key2, json);
+        }
+        localStorage.setItem(\`\${key2}-memo\`, memo);
+      }} />
+        <p className="ml-8">ID: {key2}</p>
+        <meta.component {...args}></meta.component>
+        <div className="p-2">
+          <TextareaAutosize className="
+              block p-2.5 w-full text-sm text-gray-900 bg-gray-50
+              rounded-lg border border-gray-300
+              focus:outline-none focus:ring-blue-300 focus:border-blue-300
+            " minRows={2} value={memo} onChange={e => setMemo(e.target.value)} placeholder="メモを入力"></TextareaAutosize>
+        </div>
+        <ModalDialog open={isOpen} text={scoreText} error={error} onCancel={() => setIsOpen(false)} onChange={(text: string) => {
+        setScoreText(text);
+      }} onOk={() => {
+        try {
+          setError("");
+          setScore(key2, scoreText);
+          setIsOpen(false);
+        } catch (e) {
+          console.error(e);
+          if (e instanceof Error) {
+            setError(e.message);
+          }
+        }
+      }} />
+      </>;
+  }
+}`,...(O=(E=p.parameters)==null?void 0:E.docs)==null?void 0:O.source}}};const H=["Example1","Example2"];export{u as Example1,p as Example2,H as __namedExportsOrder,T as default};
+//# sourceMappingURL=1_ProjectScoreExamples.stories-fLGLiIn7.js.map
---
+++ assets/3_ProjectScoreDarkMode.stories-Dn9gVRxp.js
@@ -0,0 +1,35 @@
+import{j as r}from"./jsx-runtime-DFnSfiB4.js";import{P}from"./ProjectScore-BDHpq3hS.js";import"./index-DQ2WTIsS.js";import"./Comment-CLRktOpt.js";import"./EightElements-CK4dmmlp.js";import"./Measure-KdatraD6.js";import"./index-Ce-rDAX5.js";import"./IntermediatePurpose-CeZnclaa.js";import"./ObjectiveBox-DqMJMEJU.js";const h={title:"pufu-editor/ProjectScoreDarkMode",component:P,parameters:{layout:"fullscreen"},tags:["autodocs"]},e={args:{dark:!0}},a={args:{feedback:!0,dark:!0}},o={args:{preview:!0,dark:!0}},s={args:{lang:"en",dark:!0},render:function({...E}){return r.jsxs(r.Fragment,{children:[r.jsx("p",{children:"※Docsでは英語表示不可"}),r.jsx(h.component,{...E})]})}},t={args:{feedback:!0,mobile:!0,dark:!0}};var n,c,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    dark: true
+  }
+}`,...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,u,d;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    feedback: true,
+    dark: true
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var i,g,l;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    preview: true,
+    dark: true
+  }
+}`,...(l=(g=o.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var k,f,b;s.parameters={...s.parameters,docs:{...(k=s.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    lang: "en",
+    dark: true
+  },
+  render: function Comp({
+    ...args
+  }) {
+    return <>
+        <p>※Docsでは英語表示不可</p>
+        <meta.component {...args}></meta.component>
+      </>;
+  }
+}`,...(b=(f=s.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var j,x,S;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    feedback: true,
+    mobile: true,
+    dark: true
+  }
+}`,...(S=(x=t.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};const q=["Edit","Feedback","Preview","English","Mobile"];export{e as Edit,s as English,a as Feedback,t as Mobile,o as Preview,q as __namedExportsOrder,h as default};
+//# sourceMappingURL=3_ProjectScoreDarkMode.stories-Dn9gVRxp.js.map
