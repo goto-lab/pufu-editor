d59df2548afa72a8a80af2e0ac4ec375be2070c9
dyson-yamashita <dyson-yamashita@users.noreply.github.com>
2026-06-05 03:38:59 +0000

    deploy: 32383a9c840ded748ddfc6aac0fc23afd28871b0

---
+++ assets/3_ProjectScoreDarkMode.stories-8-Z20oq7.js
@@ -0,0 +1,35 @@
+import{j as r}from"./jsx-runtime-BjG_zV1W.js";import{P}from"./ProjectScore-BvWfVLZ0.js";import"./index-DuTGsLYU.js";import"./Comment-C-fCqjML.js";import"./EightElements-HRbJOXoV.js";import"./Measure-Cs4Wp05Z.js";import"./index-CB7vYGjU.js";import"./IntermediatePurpose-B-Aoy9P-.js";import"./ObjectiveBox-BWPcIHaJ.js";const h={title:"pufu-editor/ProjectScoreDarkMode",component:P,parameters:{layout:"fullscreen"},tags:["autodocs"]},e={args:{dark:!0}},a={args:{feedback:!0,dark:!0}},o={args:{preview:!0,dark:!0}},s={args:{lang:"en",dark:!0},render:function({...E}){return r.jsxs(r.Fragment,{children:[r.jsx("p",{children:"※Docsでは英語表示不可"}),r.jsx(h.component,{...E})]})}},t={args:{feedback:!0,mobile:!0,dark:!0}};var n,c,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    dark: true
+  }
+}`,...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,u,d;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    feedback: true,
+    dark: true
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var i,g,l;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    preview: true,
+    dark: true
+  }
+}`,...(l=(g=o.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var k,f,b;s.parameters={...s.parameters,docs:{...(k=s.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    lang: "en",
+    dark: true
+  },
+  render: function Comp({
+    ...args
+  }) {
+    return <>
+        <p>※Docsでは英語表示不可</p>
+        <meta.component {...args}></meta.component>
+      </>;
+  }
+}`,...(b=(f=s.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var j,x,S;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    feedback: true,
+    mobile: true,
+    dark: true
+  }
+}`,...(S=(x=t.parameters)==null?void 0:x.docs)==null?void 0:S.source}}};const q=["Edit","Feedback","Preview","English","Mobile"];export{e as Edit,s as English,a as Feedback,t as Mobile,o as Preview,q as __namedExportsOrder,h as default};
+//# sourceMappingURL=3_ProjectScoreDarkMode.stories-8-Z20oq7.js.map
